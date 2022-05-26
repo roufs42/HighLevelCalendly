eab76ef819aa6806e01da8a76c995aa9e1288885
Rouf <roufs42@gmail.com>
2022-05-26 12:47:57 +0530

    conditional checks and refactored

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -38,19 +38,25 @@ const isValidDate = (date)=>{
 const addEvent = async (req, res, next) => {
     try {
         const data = req.body;
-        if(!isValidDate(data.datetime)){
+        let datetime = data.datetime;
+        let duration = data.duration;
+        let timezone = config.timezone;
+        if(!momentTz.tz.zone(timezone)){
+            res.status(422).send('Invalid timezone in config file');  
+        }else if(!isValidDate(datetime)){
             res.status(422).send('Input date is Invalid');  
-        }else if(Number.isNaN(data.duration)){
+        }else if(Number.isNaN(duration)){
             res.status(422).send('Input number for duration');
         }else{
             let slotsArr = Object.keys(slots);
-            let startTime =  momentTz.tz(slotsArr[0],config.timezone).utc();
-            let endTime = momentTz.tz(slotsArr[slotsArr.length-1],config.timezone).utc();
-            let utcTime = momentTz.tz(data.datetime,config.timezone).utc();
+            let startTime =  momentTz.tz(slotsArr[0],timezone).utc();
+            let endTime = momentTz.tz(slotsArr[slotsArr.length-1],timezone).utc();
+            let utcTime = momentTz.tz(datetime,timezone).utc();
     
             if(!(utcTime.isSameOrAfter(startTime) && utcTime.isSameOrBefore(endTime))){
                 res.status(422).send('DateTime is not in the range of Freeslots');
-            }else if(isValid(data.datetime , data.timezone,data.duration)){
+            }else if(isValid(datetime , data.timezone,duration)){
+                data.datetime = momentTz.tz(datetime,timezone).format();
                 await firestore.collection('events').doc().set(data);
                 let cachedData = await cache.getData();
                 slots = cachedData.slots;
@@ -73,25 +79,29 @@ const getAllEvents = async (req, res, next) => {
         let endTime = req.query.endDate;
         if(!isValidDate(startTime) || !isValidDate(endTime)){
             res.status(422).send('Input date is Invalid');  
+        }else{
+            let timezone = req.query.timezone || config.timezone;
+            let eventsArr = Object.keys(events)
+            
+            if(startTime && endTime){
+                startTime =  moment.tz(startTime,timezone);
+                endTime = moment.tz(endTime,timezone);
+                let currentTime
+                eventsArr = eventsArr.sort((a,b)=>{return new Date(a) - new Date(b)})
+                eventsArr = eventsArr.filter((ev)=>{
+                     currentTime =  moment.tz(ev,timezone);
+                     return (currentTime.isSameOrAfter(startTime) && currentTime.isSameOrBefore(endTime))
+                })
+              
+                eventsArr = eventsArr.map((ev)=>{
+                     return moment.tz(ev,timezone).format('MMMM Do YYYY, h:mm:ss a');
+               })
+                
+            }
+            res.send(eventsArr)
         }
             
-        let timezone = req.query.timezone || config.timezone;
-        let eventsArr = Object.keys(events)
-        
-        if(startTime && endTime){
-            startTime =  moment.tz(startTime,timezone);
-            endTime = moment.tz(endTime,timezone);
-            let currentTime
-            eventsArr = eventsArr.filter((ev)=>{
-                 currentTime =  moment.tz(ev,timezone);
-                 return (currentTime.isSameOrAfter(startTime) && currentTime.isSameOrBefore(endTime))
-            })
-
-            eventsArr = eventsArr.map((ev)=>{
-                 return moment.tz(ev,timezone).format('MMMM Do YYYY, h:mm:ss a');
-           })
-        }
-        res.send(eventsArr)
+    
     } catch (error) {
         res.status(400).send(error.message);
     }
@@ -105,17 +115,28 @@ const getFreeSlots = async (req, res, next) => {
         let date = ((req.query) || {}).date;
         let slotsArr = Object.keys(slots);
        
-        if(date){
-            slotsArr = slotsArr.filter((slot)=>{
-                return momentTz.tz(slot,timezone).format('YYYY DD MM') == moment(date).format('YYYY DD MM')
-            });
+        //check if given timezone is valid or not
+        if(!momentTz.tz.zone(timezone)){
+            res.status(422).send('Given Timezone is Invalid');
+        }else if(!isValidDate(date)){
+            res.status(422).send('Input date is Invalid');  
+        }else{
+            
+            if(date){
+                slotsArr = slotsArr.filter((slot)=>{
+                    return momentTz.tz(slot,timezone).format('YYYY DD MM') == moment(date).format('YYYY DD MM')
+                });
+            }
+
+            slotsArr = slotsArr.map((slot)=>{
+                return momentTz.tz(slot,timezone).format('MMMM Do YYYY h:mm:ss a');
+            })
+        
+            res.send(slotsArr);
         }
 
-        slotsArr = slotsArr.map((slot)=>{
-            return momentTz.tz(slot,timezone).format('h:mm:ss a');
-        })
-      
-        res.send(slotsArr);
+
+
     } catch (error) {
         res.status(400).send(error.message);
     }
